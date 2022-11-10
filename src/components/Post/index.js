0e4843090d1de47af2b56e7ba6454ca0d78281ce
Cristhyan <cristhyancalderon@hotmail.co.uk>
2022-11-10 16:36:45 +0000

    Task 5ish

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 import './index.css';
 
-function Post({postId, title, date, author, highlights, image}) {
-  return <article></article>;
+
+function Post(props) {
+  return <article>
+  <h2>{props.title}</h2>
+    <h3>{props.date},{props.author}</h3>
+      <p>{props.text}</p>
+  </article>;
 }
 
 export default Post;
